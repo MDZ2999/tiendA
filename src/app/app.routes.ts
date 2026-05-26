a9541302d36f233a9b9860b0dc16dac87723fe4a
Pollo28-24 <maelu080@gmail.com>
2026-05-26 07:43:37 -0600

    feat: creacion del componente products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { homeRoute } from './routes/home.route';
+import { Products } from './pages/components/products/products';
 
 export const routes: Routes = [
   homeRoute,
+
+     {
+        path: 'products',
+        component: Products,
+    },
 ];
